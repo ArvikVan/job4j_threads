9cd7cb0fc6928972f68ba1bd55517ecacfe1fd21
ArvikVan <arvikvan@gmail.com>
2021-11-12 22:35:16 +0300

    1. Запуск нити. Thread#start() [#1016]

---
+++ src/main/java/concurrent/ConcurrentOutput.java
@@ -0,0 +1,24 @@
+package concurrent;
+
+/**
+ * @author arvikv
+ * @version 1.0
+ * @since 12.11.2021
+ */
+
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("----");
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
