cb2948f2b3e1855e4dbffabf49df02c208831c07
ArvikVan <arvikvan@gmail.com>
2021-11-16 00:11:01 +0300

    0. Что такое атомарность? [#6856]

---
+++ src/main/java/thread/Count.java
@@ -0,0 +1,20 @@
+package thread;
+
+/**
+ * @author arvikv
+ * @version 1.0
+ * @since 15.11.2021
+ */
+
+
+public class Count {
+  private int value;
+
+  public void increment() {
+    value++;
+  }
+
+  public int get() {
+    return value;
+  }
+}
