befc843bd226ca4090cc93b5fc0523d42fac53ac
ArvikVan <arvikvan@gmail.com>
2021-11-16 15:25:02 +0300

    3. Immutable объекты [#267918 #229122]fix_1.1

--- src/main/java/linked/Node.java
+++ src/main/java/linked/Node.java
@@ -2,15 +2,16 @@ package linked;
 
 /**
  * @author arvikv
- * @version 1.0
+ * @version 1.1
  * @since 16.11.2021
  * Правила создания Immutable объекта.
  * 1. Все поля отмечены final.
  * 2. Состояние объекта не изменяется после создания объекта.
  * Поля отметил final
  * убрал сеттеры позволяющие менять состояние объекта
+ * 1,1 immutable это еще и запрет наследования, используйте final на уровне класса
  */
-public class Node<T> {
+public final class Node<T> {
     private final Node<T> next;
     private final T value;
 
