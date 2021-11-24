59d32344d1c9ee03bfdb939e8248619213e0bc3a
ArvikVan <arvikvan@gmail.com>
2021-11-24 14:28:55 +0300

    1. Реализовать ThreadPool [#1099 #232198]fix 1.1

--- src/main/java/pool/ThreadPool.java
+++ src/main/java/pool/ThreadPool.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 /**
  * @author ArvikV
- * @version 1.0
+ * @version 1.1
  * @since 23.11.2021
+ * 1.1
+ * 1. Это должно быть быть в конструкторе
+ * и при этом каждую нить надо запустить
+ * 2. Добавьте хотя бы main() и проверьте работу пула
  */
 public class ThreadPool {
     private final int size = Runtime.getRuntime().availableProcessors();
@@ -15,29 +19,41 @@ public class ThreadPool {
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
 
     /**
-     * метод добавляет задачи в блокирующую очередь tasks
-     * @param job на входе задача которую надо добавить в блок.очередь
-     * @throws InterruptedException исключение вынесли в метод
      * добавляем задачи tasks.offer(job); потоков в кол-ве size согласно условию
      * Проверяем состояние флага Thread.currentThread().isInterrupted()
      * возвращаем объект из внутренней коллекции
      * заполняем жобами блок.очередь
+     * и при этом каждую нить надо запустить
+     */
+    public ThreadPool() throws InterruptedException {
+        for (int i = 0; i < size; i++) {
+            threads.add(new Thread(
+                    () -> {
+                        try {
+                            while (!Thread.currentThread().isInterrupted()) {
+                                tasks.poll().run();
+                            }
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+            ));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+            thread.join(1000);
+
+        }
+    }
+
+    /**
+     * метод добавляет задачи в блокирующую очередь tasks
+     * @param job на входе задача которую надо добавить в блок.очередь
+     * @throws InterruptedException исключение вынесли в метод
      *
      */
+
     public void work(Runnable job) throws InterruptedException {
-            for (int i = 0; i < size; i++) {
-                threads.add(new Thread(
-                        () -> {
-                            try {
-                                while (!Thread.currentThread().isInterrupted()) {
-                                   tasks.poll().run();
-                                }
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                            }
-                        }
-                ));
-            }
         tasks.offer(job);
     }
 
@@ -48,5 +64,13 @@ public class ThreadPool {
         for (Thread thread : threads) {
             thread.interrupt();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        threadPool.work(() -> System.out.println("first"));
+        threadPool.work(() -> System.out.println("second"));
+        threadPool.work(() -> System.out.println("third"));
+        threadPool.shutdown();
     }
 }
