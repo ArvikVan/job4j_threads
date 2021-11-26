e67408629cafb3c80f227cd0047922665f7e1299
ArvikVan <arvikvan@gmail.com>
2021-11-26 21:32:19 +0300

    3. ForkJoinPool [#315067 #232710]fix_1.4

--- src/main/java/pool/ParallelFind.java
+++ src/main/java/pool/ParallelFind.java
@@ -1,5 +1,6 @@
 package pool;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 /**
@@ -12,14 +13,21 @@ import java.util.concurrent.RecursiveTask;
  *  1.2 ваша текущая реализация в определенных случаях получает значение индекса -1, чего быть не должно
  *  1.3 в цикле i < to;
  *  rightSort = new ParallelMergeSort(array, mid + 1, to - 1, findMe); так (array, mid, to, findMe)
+ *  1.4
+ *  3. Переименуйте класс, а то складывается впечатление что он для сортировки предназначен
+ *  1. чтобы 2 раза с одного и того же не начинать Все же тут добавьте +1
+ *  ParallelFind rightSort = new ParallelFind(array, mid + 1, to, findMe);
+ *  2. Добавьте в класс статический метод, который будет принимать все данные для поиска
+ *  и в этом методе будете вызывать invoke, а не в тестах
+ *
  */
-public class ParallelMergeSort extends RecursiveTask<Integer> {
+public class ParallelFind extends RecursiveTask<Integer> {
     private final int[] array;
     private final int from;
     private final int to;
     private final int findMe;
 
-    public ParallelMergeSort(int[] array, int from, int to, int findMe) {
+    public ParallelFind(int[] array, int from, int to, int findMe) {
         this.array = array;
         this.from = from;
         this.to = to;
@@ -37,7 +45,7 @@ public class ParallelMergeSort extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if (to - from <= 10) {
-            for (int i = from; i < to; i++) {
+            for (int i = from; i <= to; i++) {
                 if (array[i] == findMe) {
                     return i;
                 }
@@ -45,12 +53,23 @@ public class ParallelMergeSort extends RecursiveTask<Integer> {
             return -1;
         }
         int mid = (from + to) / 2;
-        ParallelMergeSort leftSort = new ParallelMergeSort(array, from, mid, findMe);
-        ParallelMergeSort rightSort = new ParallelMergeSort(array, mid, to, findMe);
+        ParallelFind leftSort = new ParallelFind(array, from, mid, findMe);
+        ParallelFind rightSort = new ParallelFind(array, mid + 1, to - 1, findMe);
         leftSort.fork();
         rightSort.fork();
         int left = leftSort.join();
         int right = rightSort.join();
         return Math.max(left, right);
+    }
+
+    /**
+     * Метод в котором запускаем выполнение
+     * @param array массив на входе
+     * @param findMe значение которое надо найти
+     * @return на выходе индекс найденого
+     */
+    public static Integer search(int[] array, int findMe) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new ParallelFind(array, 0, array.length - 1, findMe));
     }
 }
--- src/test/java/pool/ParallelFindTest.java
+++ src/test/java/pool/ParallelFindTest.java
@@ -12,37 +12,31 @@ import static org.junit.Assert.*;
  * @version 1.0
  * @since 25.11.2021
  */
-public class ParallelMergeSortTest {
+public class ParallelFindTest {
     @Test
     public void whenArraylessThen10() {
         int[] array = {11, 12, 13, 14, 15, 16, 17};
-        ParallelMergeSort parallelMergeSort = new ParallelMergeSort(
+        ParallelFind parallelMergeSort = new ParallelFind(
                 array, 0, array.length, 16);
-        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
-        int index = forkJoinPool.invoke(parallelMergeSort);
-        assertThat(5, is(index));
+        assertThat(5, is(ParallelFind.search(array, 16)));
     }
 
     @Test
     public void whenArrayMoreThen10() {
         int[] array = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-        ParallelMergeSort parallelMergeSort = new ParallelMergeSort(
+        ParallelFind parallelMergeSort = new ParallelFind(
                 array, 0, array.length, 19
         );
-        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
-        int index = forkJoinPool.invoke(parallelMergeSort);
-        assertThat(8, is(index));
+        assertThat(8, is(ParallelFind.search(array, 19)));
     }
 
     @Test
     public void whenReturnMinusOne() {
         int[] array = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-        ParallelMergeSort parallelMergeSort = new ParallelMergeSort(
+        ParallelFind parallelMergeSort = new ParallelFind(
                 array, 0, array.length, 17
         );
-        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
-        int index = forkJoinPool.invoke(parallelMergeSort);
-        assertThat(6, is(index));
+        assertThat(6, is(ParallelFind.search(array, 17)));
     }
 
 }
